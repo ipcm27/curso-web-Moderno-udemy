488933e357ab3183871f4ff89833f788062ebb56
Igor Pierre <igorpcmiranda@gmail.com>
2021-10-18 12:15:16 -0300

    no message

--- Seção 6 - functions/95 - Arrow Functions.js
+++ Seção 6 - functions/95 - Arrow Functions.js
@@ -1,7 +1,3 @@
-// EC6 2015- introduzida as funções arrow, que são boa sproque:
-//  1) mais curtas, sintaxe mais reduzida - func'ões especialista respeitando single responsibility principle
-//  2) this associados ao contexto que a funçao foi escrita
-
 // NORMAL
 let dobro = function (a) {
   return 2 * a;
@@ -15,7 +11,8 @@ let dobro = (a) => {
 // Arrow com 1 parâmetro apenas
 let dobro = (a) => 2 * a;
 
-//arrow em funções de alta órdem:
+// Arrow em funções de alta órdem
+
 const nums = [1, 2, 3, 4, 5];
 
 // com arrow
@@ -25,3 +22,7 @@ let resultado = nums.map((a) => 2 * a);
 let resultado2 = nums.map(function (a) {
   return 2 * a;
 });
+
+// EC6 2015- introduzida as funções arrow, que são boa sproque:
+//  1) mais curtas, sintaxe mais reduzida - func'ões especialista respeitando single responsibility principle
+//  2) this associados ao contexto que a funçao foi escrita
